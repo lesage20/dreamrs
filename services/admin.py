0300500c2b12b24c2a49553bee7bff3206262ac2
angezanou00@gmail.com0 <angezanou00@gmail.com>
2020-06-17 21:48:20 +0200

    all done.

--- services/admin.py
+++ services/admin.py
@@ -1,3 +1,23 @@
 from django.contrib import admin
 
 # Register your models here.
+from .import models
+
+@admin.register(models.Apartment)
+class ApartmentAdmin(admin.ModelAdmin):
+    list_display = ['type_apartment', ]
+    search_field = ['type_apartement']
+
+
+@admin.register(models.Service)
+class ServicetAdmin(admin.ModelAdmin):
+    list_display = ['nom', 'image']
+    search_field = ['nom']
+    ordering = ['nom', 'date_add']
+
+
+@admin.register(models.Project)
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ['domaine','titre', 'nouveau_projet']
+    search_display = ['domaine', 'nouveau_projet']
+    ordering = ['domaine', 'titre']
--- services/models.py
+++ services/models.py
@@ -1,3 +1,60 @@
 from django.db import models
 
 # Create your models here.
+
+class Service(models.Model):
+
+    nom = models.CharField(max_length = 255)
+    description = models.TextField()
+    image = models.ImageField(upload_to='images/Service')
+
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Service'
+        verbose_name_plural = 'Services'
+
+    def __str__(self):
+        return self.nom
+
+
+
+class Project(models.Model):
+    
+    domaine = models.CharField(max_length = 200)
+    titre = models.CharField(max_length = 200)
+    nouveau_projet = models.BooleanField(default=True)
+    
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+        verbose_name = 'Project'
+        verbose_name_plural = 'Projects'
+
+    def __str__(self):
+        self.titre
+
+
+class Apartment(models.Model):
+    
+    type_apartment = models.CharField(max_length = 150)
+    description    = models.TextField()
+    nombre_salle_bain = models.PositiveIntegerField()
+    nombre_chambre = models.PositiveIntegerField()
+    nombre_fenetre = models.PositiveIntegerField()
+
+    date_add = models.DateTimeField(auto_now_add=True)
+    date_update = models.DateTimeField(auto_now=True)
+    status = models.BooleanField(default=True)
+
+    class Meta:
+
+        verbose_name = 'Apartment'
+        verbose_name_plural = 'Apartments'
+
+    def __str__(self):
+        return self.type_apartment
